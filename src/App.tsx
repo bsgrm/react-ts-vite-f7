d83dabc6e4d9e66a88f5659e1c81509e68378e81
Raymond Lin <raymondlin@live.hk>
2022-01-26 01:02:30 +0800

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,83 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+// import { useState } from 'react'
+// import logo from './logo.svg'
+// import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React, { Component } from 'react';
+import { App, View } from 'framework7-react';
+import Login from './Login';
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>Hello Vite + React!</p>
+//         <p>
+//           <button type="button" onClick={() => setCount((count) => count + 1)}>
+//             count is: {count}
+//           </button>
+//         </p>
+//         <p>
+//           Edit <code>App.tsx</code> and save to test HMR updates.
+//         </p>
+//         <p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//           {' | '}
+//           <a
+//             className="App-link"
+//             href="https://vitejs.dev/guide/features.html"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Vite Docs
+//           </a>
+//         </p>
+//       </header>
+//     </div>
+//   )
+// }
+
+interface AppProps {}
+interface AppState {
+  name: string;
 }
 
-export default App
+const f7params = {
+  id: 'io.framework7.testapp', // App bundle ID
+  name: 'Framework7', // App name
+  theme: 'auto', // Automatic theme detection
+  // store,
+  // App routes
+  routes: [
+    {
+      path: '/',
+      component: Login,
+    },
+  ],
+};
+
+export default class Application extends Component<AppProps, AppState> {
+  constructor(props : any) {
+    super(props);
+    this.state = {
+      name: 'React',
+    };
+  }
+
+  render() {
+    return (
+      <App {...f7params}>
+        <View main url="/" />
+      </App>
+    );
+  }
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
+
+// Import Framework7
+import Framework7 from 'framework7/lite-bundle';
+
+// Import Framework7-React Plugin
+import Framework7React from 'framework7-react';
+
+// Import Framework7 Styles
+import 'framework7/framework7-bundle.css';
+
+// Import Icons and App Custom Styles
+import './index.less'
+
+// Import App Component
 import App from './App'
+
+// Init F7 React Plugin
+Framework7.use(Framework7React)
 
 ReactDOM.render(
   <React.StrictMode>
